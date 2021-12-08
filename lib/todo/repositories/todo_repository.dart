f60a55f8e764362ed2a448eb04dd01614ac3a0e4
nohli <43643339+nohli@users.noreply.github.com>
2021-12-08 20:31:00 +0100

    Create todo repository for downloading the todo list

---
+++ lib/todo/repositories/todo_repository.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:http/http.dart' as http;
+
+import 'package:todo/todo/models/todo_list.dart';
+
+class TodoRepository {
+  Future<TodoList> getTodos() async {
+    const url = 'https://jsonplaceholder.typicode.com/todos';
+
+    final response = await http.get(Uri.parse(url));
+    final statusCode = response.statusCode;
+
+    if (statusCode == HttpStatus.ok) {
+      final list = jsonDecode(response.body);
+      final TodoList todos = TodoList.fromJson(list);
+      return todos;
+    } else {
+      return TodoList([]);
+    }
+  }
+}
