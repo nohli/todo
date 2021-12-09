d5ef092fb672405d84d53f0bd0b369942c9349b4
nohli <43643339+nohli@users.noreply.github.com>
2021-12-09 02:04:13 +0100

    Improve UI

--- lib/todo/widgets/add_todo_item_widget.dart
+++ lib/todo/widgets/add_todo_item_widget.dart
@@ -29,7 +29,7 @@ class _AddTodoItemWidgetState extends State<AddTodoItemWidget> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 12),
+      padding: const EdgeInsets.symmetric(horizontal: 8),
       child: Row(
         children: [
           Expanded(
