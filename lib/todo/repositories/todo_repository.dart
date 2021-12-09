c22cda0ecf38312a4aa21de77174356b8d07c4b6
nohli <43643339+nohli@users.noreply.github.com>
2021-12-09 03:41:04 +0100

    Refactor TodoRepository.getTodoList()

--- lib/todo/repositories/todo_repository.dart
+++ lib/todo/repositories/todo_repository.dart
@@ -1,4 +1,3 @@
-import 'dart:convert';
 import 'dart:io';
 
 import 'package:http/http.dart' as http;
@@ -6,16 +5,14 @@ import 'package:http/http.dart' as http;
 import 'package:todo/todo/models/todo_list.dart';
 
 class TodoRepository {
-  static Future<TodoList> getTodoList() async {
+  Future<TodoList> getTodoList() async {
     const url = 'https://jsonplaceholder.typicode.com/todos';
 
     final response = await http.get(Uri.parse(url));
     final statusCode = response.statusCode;
 
     if (statusCode == HttpStatus.ok) {
-      final list = jsonDecode(response.body);
-      final TodoList todos = TodoList.fromJson(list);
-      return todos;
+      return TodoList.fromJson(response.body);
     }
 
     return TodoList.empty();
