0fbd41d1a20865570ec76d0bf15b310b63a38a3f
nohli <43643339+nohli@users.noreply.github.com>
2021-12-09 01:14:17 +0100

    Create TodoListWidget for showing the text field and all todo items

---
+++ lib/todo/widgets/todo_list_widget.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:todo/todo/providers/todo_provider.dart';
+import 'package:todo/todo/widgets/add_todo_item_widget.dart';
+import 'package:todo/todo/widgets/todo_item_widget.dart';
+
+class TodoListWidget extends ConsumerWidget {
+  const TodoListWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final todos = ref.watch(todosProvider).todos;
+    final provider = ref.read(todosProvider.notifier);
+    if (todos.isEmpty) provider.getTodos();
+
+    return SafeArea(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          AddTodoItemWidget(provider: provider),
+          Expanded(
+            child: ListView.builder(
+              padding: const EdgeInsets.fromLTRB(4, 6, 4, 6),
+              itemCount: todos.length,
+              itemBuilder: (context, index) {
+                return TodoItemWidget(
+                  todos[index],
+                  provider: provider,
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
