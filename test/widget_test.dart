476f7100c828b05a803f5f2205f29bdccc463412
nohli <43643339+nohli@users.noreply.github.com>
2021-12-09 22:36:15 +0100

    Delete widget_test.dart

--- test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:todo/main.dart';
-
-void main() {
-  testWidgets('Test', (WidgetTester tester) async {
-    await tester.pumpWidget(const MyApp());
-  });
-}
