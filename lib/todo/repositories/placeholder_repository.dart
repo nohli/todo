1cf987b6168239369d84105e091846c3a4d82b7c
nohli <43643339+nohli@users.noreply.github.com>
2021-12-09 04:49:31 +0100

    Add http post request to PlaceholderRepository

--- lib/todo/repositories/placeholder_repository.dart
+++ lib/todo/repositories/placeholder_repository.dart
@@ -1,20 +1,39 @@
 import 'dart:io';
 
+import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 
 import 'package:todo/todo/models/todo_list.dart';
 
 class PlaceholderRepository {
-  Future<TodoList> getTodoList() async {
-    const url = 'https://jsonplaceholder.typicode.com/todos';
+  static const _url = 'https://jsonplaceholder.typicode.com/todos';
+  final _uri = Uri.parse(_url);
 
-    final response = await http.get(Uri.parse(url));
+  Future<TodoList> getTodoList() async {
+    final response = await http.get(_uri);
     final statusCode = response.statusCode;
 
     if (statusCode == HttpStatus.ok) {
       return TodoList.fromJson(response.body);
     }
-
     return TodoList.empty();
+  }
+
+  Future<void> saveTodoList(TodoList list) async {
+    final response = await http.post(
+      _uri,
+      body: list.toJson(),
+    );
+
+    final statusCode = response.statusCode;
+
+    if (statusCode == HttpStatus.created) {
+      return;
+    }
+    if (kDebugMode) {
+      throw Exception(
+        'Failed to send todo list to api. Statuscode: $statusCode',
+      );
+    }
   }
 }
