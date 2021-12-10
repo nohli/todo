b4ef7ba7456cc2d1448ee1946315849685f03a25
nohli <43643339+nohli@users.noreply.github.com>
2021-12-10 14:13:10 +0100

    Add trailing comma

--- test/todo/object_mothers/todo_list_mother.dart
+++ test/todo/object_mothers/todo_list_mother.dart
@@ -4,26 +4,28 @@ import 'package:todo/todo/models/todo_item.dart';
 import 'package:todo/todo/models/todo_list.dart';
 
 class TodoListMother {
-  static final json = jsonEncode([
-    {
-      'userId': 1,
-      'id': 1,
-      'title': 'delectus aut autem',
-      'completed': false,
-    },
-    {
-      'userId': 1,
-      'id': 2,
-      'title': 'quis ut nam facilis et officia qui',
-      'completed': false,
-    },
-    {
-      'userId': 2,
-      'id': 3,
-      'title': 'fugiat veniam minus',
-      'completed': true,
-    }
-  ]);
+  static final json = jsonEncode(
+    [
+      {
+        'userId': 1,
+        'id': 1,
+        'title': 'delectus aut autem',
+        'completed': false,
+      },
+      {
+        'userId': 1,
+        'id': 2,
+        'title': 'quis ut nam facilis et officia qui',
+        'completed': false,
+      },
+      {
+        'userId': 2,
+        'id': 3,
+        'title': 'fugiat veniam minus',
+        'completed': true,
+      }
+    ],
+  );
 
   static final todoList = TodoList(
     [
